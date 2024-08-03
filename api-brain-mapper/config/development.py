5a6ff6dcbccbb40e1852ba6ba6f91a2fdcd95f60
SenpaiSuchil <miguel.rplascencia@gmail.com>
2024-08-03 15:36:08 -0600

    added mariadb to docker compose

--- api-brain-mapper/config/development.py
+++ api-brain-mapper/config/development.py
@@ -6,5 +6,5 @@ load_dotenv()
 class DevelopmentConfig(object):
     TESTING = False
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACKS_MODIFICATIONS = False
