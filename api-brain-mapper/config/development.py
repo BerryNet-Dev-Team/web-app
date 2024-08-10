bf787fe13149338da67b995e03e42f456a128661
SenpaiSuchil <miguel.rplascencia@gmail.com>
2024-08-10 13:00:08 -0600

    added flask to dockerfile again

--- api-brain-mapper/config/development.py
+++ api-brain-mapper/config/development.py
@@ -6,6 +6,6 @@ load_dotenv()
 class DevelopmentConfig(object):
     TESTING = False
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlclientconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}/{os.getenv('DB_NAME')}"
 
     SQLALCHEMY_TRACKS_MODIFICATIONS = False
