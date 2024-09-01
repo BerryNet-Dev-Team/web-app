8d3ff44c1b1bd57051be5c16b4ce29a49db7e92d
SenpaiSuchil <miguel.rplascencia@gmail.com>
2024-08-31 23:17:04 -0600

    fixed typo

---
+++ init_template copy.sql
@@ -0,0 +1,10 @@
+-- init.sql
+CREATE DATABASE IF NOT EXISTS mi_base_de_datos;
+
+CREATE USER 'user1'@'%' IDENTIFIED BY 'password1';
+GRANT ALL PRIVILEGES ON mi_base_de_datos.* TO 'user1'@'%';
+
+CREATE USER 'user2'@'%' IDENTIFIED BY 'password2';
+GRANT ALL PRIVILEGES ON mi_base_de_datos.* TO 'user2'@'%';
+
+FLUSH PRIVILEGES;
