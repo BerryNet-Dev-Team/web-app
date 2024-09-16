10848c0ad196d8e1ae883319063427512e1a8498
Brandon Hernandez Ledezma <hernandezledezma.b@gmail.com>
2024-09-16 13:42:21 -0600

    fix: Made a lil correction in the postgres init script, now it grant ALTER permissions and set a password to initial user

--- initPg_template.sh
+++ initPg_template.sh
@@ -2,7 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER docker;
+	CREATE USER docker WITH PASSWORD 'password1';
 	CREATE DATABASE docker;
 	GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+	ALTER DATABASE docker OWNER TO docker;
 EOSQL
