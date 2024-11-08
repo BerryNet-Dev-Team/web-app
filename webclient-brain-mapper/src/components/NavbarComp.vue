cb9ec49642d8b872368bd19920e97111b745bf7f
DannyB935 <daniel.bustos935@hotmail.com>
2024-11-07 19:36:03 -0600

    feat(webclient): Added methods on navbar to move between pages

--- webclient-brain-mapper/src/components/NavbarComp.vue
+++ webclient-brain-mapper/src/components/NavbarComp.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center justify-around gap-4">
       <img src="/src/assets/LogoTemp.png" alt="Icono" class="h-16 max-h-16"/>
       <v-btn variant="text">{{ $t('navbar.start') }}</v-btn>
-      <v-btn color="orange-darken-4">{{ $t('navbar.create') }}</v-btn>
+      <v-btn color="orange-darken-4" @click="ToCreate">{{ $t('navbar.create') }}</v-btn>
     </div>
     <div class="flex items-center justify-between">
       <v-menu>
@@ -17,7 +17,7 @@
         </template>
         <v-list bg-color="#8AC6D0">
           <v-list-item>
-            <v-btn variant="text">{{ $t('navbar.viewProfile') }}</v-btn>
+            <v-btn variant="text" @click="ToProfile">{{ $t('navbar.viewProfile') }}</v-btn>
           </v-list-item>
           <v-divider></v-divider>
           <v-list-item>
@@ -31,6 +31,14 @@
 
 <script>
   export default {
-    name: 'NavbarComp'
+    name: 'NavbarComp',
+    methods:{
+      ToCreate(){
+        this.$router.push('/create');
+      },
+      ToProfile(){
+        this.$router.push('/profile');
+      }
+    }
   }
 </script>
