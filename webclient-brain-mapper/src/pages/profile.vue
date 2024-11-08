5df42bd14bcf9da57a8d2f90004f325397788ffe
DannyB935 <daniel.bustos935@hotmail.com>
2024-11-07 19:30:13 -0600

    ui(webclient): Changes on the profile page, needs to get data from database

--- webclient-brain-mapper/src/pages/profile.vue
+++ webclient-brain-mapper/src/pages/profile.vue
@@ -5,36 +5,45 @@
         <v-avatar image="@/assets/avatar.jpg" size="256" class="shadow-2xl hover:cursor-pointer">
         </v-avatar>
       </div>
-      <v-icon
-        icon="mdi-pencil"
-        class="bg-whiteaux p-5 rounded-lg m-3 hover:cursor-pointer transition delay-75 hover:scale-105"
-      >
-      </v-icon>
-      <p class="font-bold text-left text-pretty text-4xl text-white p-5">
-        {{ name }}
-      </p>
+      <div class="w-full md:w-1/4">
+        <p class="font-semibold text-wrap text-6xl text-white tracking-normal p-5">
+          {{ name }}
+        </p>
+      </div>
     </div>
-    <v-card 
+    <v-card
       class="font-bold text-4xl text-center py-4"
       variant="elevated"
       color="orange-darken-1"
     >
-      Mis mapeos
+      {{ $t('default.profile.myScans') }}
     </v-card>
-    <div class="flex justify-center items-center">
-      <v-container class="w-full md:w-60">
-        <v-carousel>
-          <v-carousel-item
-            v-for="n in maps"
-            :key="n"
-            src="@/assets/brain.png"
-            alt="Preview del mapeo"
-            @click="getMapClicked(n)"
-          >
-          </v-carousel-item>
-        </v-carousel>
-      </v-container>
+
+    <div class="w-full my-5 p-5">
+      <v-row>
+        <v-col v-for="(map, index) in maps" cols="12" md="4" :key="index">
+          <v-card link color="#2E5B7D">
+            <v-card-title class="font-semibold text-center">{{map.name}}</v-card-title>
+            <v-divider></v-divider>
+            <v-img
+              src="/src/assets/brain.png"
+              height="200"
+            ></v-img>
+            <v-card-text>
+              <div class="w-full flex justify-between px-5">
+                <div class="text-left font-semibold text-xl">
+                  {{ map.date }}
+                </div>
+                <div class="font-semibold text-xl">
+                  {{ $t('default.profile.numPics') }} {{map.pictures}}
+                </div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
+
   </div>
 </template>
 
@@ -42,8 +51,14 @@
   export default {
     data() {
       return {
-        name: 'John Lasasgna',
-        maps:[1, 2, 3, 4, 5]
+        name: 'Nombre1 Nombre2 ApellidoP ApellidoM',
+        maps:[
+          { name: "Mapeo1", date: "24/10/2024", pictures: 10 },
+          { name: "Mapeo2", date: "25/10/2024", pictures: 10 },
+          { name: "Mapeo3", date: "26/10/2024", pictures: 10 },
+          { name: "Mapeo4", date: "27/10/2024", pictures: 10 },
+          { name: "Mapeo5", date: "28/10/2024", pictures: 10 }
+        ]
       }
     },
     methods:{
