8efb0f51d6ebd020565cb1ba9f1f8732e1ebabb0
Brandon Hernandez Ledezma <hernandezledezma.b@gmail.com>
2025-03-13 19:01:20 -0600

    fix(webclient): Fixed 'double navigation' error in vue router.

--- webclient-brain-mapper/src/router/index.js
+++ webclient-brain-mapper/src/router/index.js
@@ -13,7 +13,7 @@ import { useAppStore } from '@/stores/app'
 import emitter from '@/plugins/emitter'
 
 // Initializes the session before router starts navigating
-async function initializeSession(to, next, from, appStore, authStore) {
+async function initializeSession(to, from, appStore, authStore) {
   try {
     if(from === START_LOCATION) {
       await authStore.initiateAppSession();
@@ -21,63 +21,59 @@ async function initializeSession(to, next, from, appStore, authStore) {
       // If user is actually logged in, prevents navigation to login
       if (to.fullPath === '/login') {
         if (authStore.getIsSessionActive) {
-          next('/profile');
-          return;
+          return '/profile';
         }
       }
 
-      // Otherwise just go
-      next();
-      return;
+      // Otherwise just return null to just go
+      return null;
     }
   } catch (error) {
     appStore.setBackendAvailable(false);
+    return null;
   }
 }
 
 // Functions to handle some cases in the router guardian
-function handleBackendError(to, next, appStore) {
+function handleBackendError(to, appStore) {
   // If backend is dead, blocks navigation and redirects all to Err5xx page
   if (!appStore.isBackendAvailable) {
     if (to.fullPath !== '/err5xx') {
-      next({path: '/err5xx'});
-      return;
+      return '/err5xx';
     }
   } else if (to.fullPath === '/err5xx') {
-    next('/login'); // After reload, if backend is available(again) this will kick out user from error page(s)
-    return;
+    return '/login'; // After reload, if backend is available(again) this will kick out user from error page(s)
   }
+
+  // If no problem with backend send null redirection
+  return null;
 }
 
 // Function to handle protected routes and its permissions
-async function handleAuthAndPermissions(to, from, next, authStore) {
+async function handleAuthAndPermissions(to, from, authStore) {
   // Handle protected routes
   if (to.meta.requiresAuth) {
     if (await authStore.isLoggedIn()) {
       // Check if route requires admin role and if user has required role
       if(to.meta.requiresAdmin) {
-        // If is not admin, cancels navigation
+        // If is not admin, redirects to user main page
         if(!authStore.isAdmin) {
-          next(false);
-          return;
+          return '/profile';
         }
       }
 
       // If auth and role are ok, continue navigation
-      next();
-      return;
+      return null;
     }
-    else { // In case user is not logged in, show modal and logout
+    else { // In case user is not logged-in, show modal and logout
       // Emit event to app's root file
       emitter.emit('session-exp');
-      next('/login');
-      return;
+      return '/login';
     }
   }
   else { // If route are not protected & requires no authentication
-    // Just go
-    next();
-    return;
+    // Just sends no redirection
+    return null;
   }
 }
 
@@ -88,27 +84,30 @@ const router = createRouter({
 })
 
 // Defines router guardian
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to, from) => {
   // Set instance of authStore & appStore
   const authStore = useAuthStore();
   const appStore = useAppStore();
 
   // 0. Waits for init session call to start routing
-  await initializeSession(to, next, from, appStore, authStore);
+  const initRedirect = await initializeSession(to, from, appStore, authStore);
+  if (initRedirect) return initRedirect;
 
   // Normal navigation
   try {
-    // 1. Verify backend errors
-    handleBackendError(to, next, appStore);
+    // 1. Verify backend errors, and redirect to error page if required
+    const backendErrRedirect = handleBackendError(to, appStore);
+    if (backendErrRedirect) return backendErrRedirect;
   
-    // 2. Here route validations are done
+    // 2. Here route validations and redirections are done
     // Check if route needs auth and if user is authenticated
-    await handleAuthAndPermissions(to, from, next, authStore);
-  
-    // 3. If there is nothing to verify, just continue
-    // next();
+    const authRedirect = await handleAuthAndPermissions(to, from, authStore);
+    if (authRedirect) return authRedirect;
+
+    // 3. If everything ok with previous validations, just continue
   } catch (error) {
     console.error('Fatal error in navigation guard:'/*, error*/);
+    return '/';
   }
 })
 
